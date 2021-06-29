0489cf7315d978966c18c329b64d2410e5133bfd
RyderE <86691185+RyderE@users.noreply.github.com>
2021-06-30 10:05:51 +1200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,60 @@
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.showIcon(IconNames.Yes)
+})
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 15; index++) {
+        basic.showLeds(`
+            # # . # #
+            # # # # #
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        basic.pause(100)
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(100)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    basic.showIcon(IconNames.No)
+})
+for (let index = 0; index < 4; index++) {
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(100)
+}
+basic.forever(function () {
+	
+})
